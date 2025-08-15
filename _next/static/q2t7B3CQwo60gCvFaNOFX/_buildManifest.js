4b51e692ed445d8f17cadb21f5e96b6e95c3c599
DanhCypher <brucelee6788@gmail.com>
2025-08-15 08:36:36 +0700

    Deploy to GitHub Pages

---
+++ _next/static/q2t7B3CQwo60gCvFaNOFX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/960-8e24514796df3565.js","static/chunks/pages/index-5690ebac9bf98c9d.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ src/data/data.tsx
@@ -0,0 +1,316 @@
+import {
+  AcademicCapIcon,
+  ArrowDownTrayIcon,
+  CalendarIcon,
+  FlagIcon,
+  MapIcon,
+  SparklesIcon,
+} from '@heroicons/react/24/outline';
+
+import FacebookIcon from '../components/Icon/FacebookIcon';
+import GithubIcon from '../components/Icon/GithubIcon';
+import heroImage from '../images/header-background.webp';
+import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
+import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
+import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
+import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
+import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
+import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
+import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
+import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
+import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
+import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
+import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
+import profilepic from '../images/profilepic.jpg';
+import testimonialImage from '../images/testimonial.webp';
+import {
+  About,
+  ContactSection,
+  ContactType,
+  Hero,
+  HomepageMeta,
+  PortfolioItem,
+  SkillGroup,
+  Social,
+  TestimonialSection,
+  TimelineItem,
+} from './dataDef';
+
+/**
+ * Page meta data
+ */
+export const homePageMeta: HomepageMeta = {
+  title: 'React Resume Template',
+  description: "Example site built with Tim Baker's react resume template",
+};
+
+/**
+ * Section definition
+ */
+export const SectionId = {
+  Hero: 'hero',
+  About: 'about',
+  Contact: 'contact',
+  Portfolio: 'portfolio',
+  Resume: 'resume',
+  Skills: 'skills',
+  Stats: 'stats',
+  Testimonials: 'testimonials',
+} as const;
+
+export type SectionId = (typeof SectionId)[keyof typeof SectionId];
+
+/**
+ * Hero section
+ */
+export const heroData: Hero = {
+  imageSrc: heroImage,
+  name: `I'm Danh.`,
+  description: (
+    <>
+      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
+        I'm a Vietnam based <strong className="text-stone-100">Full Stack Software Engineer</strong>.
+      </p>
+      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg"></p>
+    </>
+  ),
+  actions: [
+    {
+      href: 'https://drive.google.com/file/d/1qGZXfqqcFZ4OMAhsVu5xw05US-OkAmKy/view?usp=sharing',
+      text: 'Resume',
+      primary: true,
+      Icon: ArrowDownTrayIcon,
+    },
+    {
+      href: `#${SectionId.Contact}`,
+      text: 'Contact',
+      primary: false,
+    },
+  ],
+};
+
+/**
+ * About section
+ */
+export const aboutData: About = {
+  profileImageSrc: profilepic,
+  description: `I am a fourth-year student majoring in Software Engineering, passionate about designing and developing modern digital products. I possess a solid foundation in both Frontend and Backend development, along with strong self-learning abilities and the capacity to quickly adapt to new technologies.
+Throughout my studies, I have participated in various academic and personal projects, which have helped me refine my skills in programming, requirement analysis, system design, and user experience optimization. I strive to create innovative, efficient, and scalable solutions while maintaining a mindset of continuous learning, preparing myself for a professional working environment after graduation.`,
+  aboutItems: [
+    {label: 'Location', text: 'Vo Ngoc Dung Street, Dong Hoa Ward, Di An City, Binh Duong.', Icon: MapIcon},
+    {label: 'Age', text: '21', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Vietnamese', Icon: FlagIcon},
+    {label: 'Interests', text: 'read newspaper, research new technology.', Icon: SparklesIcon},
+    {label: 'Study', text: 'Saigon International University', Icon: AcademicCapIcon},
+  ],
+};
+
+/**
+ * Skills section
+ */
+export const skills: SkillGroup[] = [
+  {
+    name: 'Spoken languages',
+    skills: [
+      {
+        name: 'English',
+        level: 5,
+      },
+    ],
+  },
+  {
+    name: 'Frontend development',
+    skills: [
+      {
+        name: 'React',
+        level: 7,
+      },
+      {
+        name: 'Typescript',
+        level: 7,
+      },
+    ],
+  },
+  {
+    name: 'Backend development',
+    skills: [
+      {
+        name: 'Node.js',
+        level: 9.5,
+      },
+      {
+        name: 'Express',
+        level: 8.5,
+      },
+    ],
+  },
+  {
+    name: 'Database',
+    skills: [
+      {
+        name: 'MongoDB',
+        level: 9,
+      },
+      {
+        name: 'MySQL',
+        level: 4,
+      },
+    ],
+  },
+];
+
+/**
+ * Portfolio section
+ */
+export const portfolioItems: PortfolioItem[] = [
+  {
+    title: 'Project title 1',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage1,
+  },
+  {
+    title: 'Project title 2',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage2,
+  },
+  {
+    title: 'Project title 3',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage3,
+  },
+  {
+    title: 'Project title 4',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage4,
+  },
+  {
+    title: 'Project title 5',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage5,
+  },
+  {
+    title: 'Project title 6',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage6,
+  },
+  {
+    title: 'Project title 7',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage7,
+  },
+  {
+    title: 'Project title 8',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage8,
+  },
+  {
+    title: 'Project title 9',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage9,
+  },
+  {
+    title: 'Project title 10',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage10,
+  },
+  {
+    title: 'Project title 11',
+    description: 'Give a short description of your project here.',
+    url: 'https://reactresume.com',
+    image: porfolioImage11,
+  },
+];
+
+/**
+ * Resume section -- TODO: Standardize resume contact format or offer MDX
+ */
+export const education: TimelineItem[] = [
+  {
+    date: '2022 - Present',
+    location: 'Saigon International University',
+    title: 'Computer science/software engineering',
+    content: <p>GPA: 3.17/4.0</p>,
+  },
+];
+
+export const experience: TimelineItem[] = [
+  {
+    date: '01/07/2024 - 25/12/2024',
+    location: 'Cohota',
+    title: 'FullStack NodeJS Developer',
+    content: (
+      <p>
+        My primary responsibility is to enhance the system's functionality by refining its performance, integrating OCR
+        character recognition, and expanding its capability to handle Matching questions effectively
+      </p>
+    ),
+  },
+];
+
+/**
+ * Testimonial section
+ */
+export const testimonial: TestimonialSection = {
+  imageSrc: testimonialImage,
+  testimonials: [
+    {
+      name: 'John Doe',
+      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
+      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
+    },
+    {
+      name: 'Jane Doe',
+      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
+      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
+    },
+    {
+      name: 'Someone else',
+      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
+      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
+    },
+  ],
+};
+
+/**
+ * Contact section
+ */
+
+export const contact: ContactSection = {
+  headerText: 'Get in touch.',
+  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  items: [
+    {
+      type: ContactType.Email,
+      text: 'brucelee6788@gmail.com',
+      href: 'mailto:brucelee6788@gmail.com',
+    },
+    {
+      type: ContactType.Facebook,
+      text: 'Lê Zanh',
+      href: 'https://www.facebook.com/danhlowkey',
+    },
+    {
+      type: ContactType.Github,
+      text: 'Danh Cypher',
+      href: 'https://github.com/DanhCypher',
+    },
+  ],
+};
+
+/**
+ * Social items
+ */
+export const socialLinks: Social[] = [
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/DanhCypher'},
+  {label: 'Facebook', Icon: FacebookIcon, href: 'https://www.facebook.com/danhlowkey'},
+];
