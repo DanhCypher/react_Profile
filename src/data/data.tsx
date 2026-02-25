95ca2d2f129b3da6de0c0f6a9916733fd0d5573e
DanhCypher <165006679+DanhCypher@users.noreply.github.com>
2026-02-25 23:38:30 +0700

    Update experience date range for FullStack Developer

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -248,7 +248,7 @@ export const education: TimelineItem[] = [
 
 export const experience: TimelineItem[] = [
   {
-    date: '27/08/2025 - Now (Remote Support)',
+    date: '27/08/2025 - 17/01/2026',
     location: 'PIX Interaction',
     title: 'FullStack NodeJS Developer',
     content: (
